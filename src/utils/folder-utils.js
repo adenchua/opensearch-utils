c691d6dd5ef24dda7b55658b24aa759825025c61
Aden Chua <37574101+adenchua@users.noreply.github.com>
2024-10-29 22:50:05 +0800

    add comments to folder utils method

--- src/utils/folder-utils.js
+++ src/utils/folder-utils.js
@@ -2,12 +2,23 @@ import fs from "fs";
 import yauzl from "yauzl";
 import path from "path";
 
+/**
+ * Creates a directory if it doesn't exist
+ * @param {string} dirName - name of the directory
+ */
 function createDir(dirName) {
   if (!fs.existsSync(dirName)) {
     fs.mkdirSync(dirName);
   }
 }
 
+/**
+ * Unzips a .zip file and write to a destination folder
+ * Executes a callback function after the files are written
+ * @param {string} srcPath - source path of the zip file
+ * @param {string} destPath - destination path for the unzipped file. Creates directory if doesn't exist
+ * @param {Function} callbackFn - callback function to execute upon unzipping content of the zipped file
+ */
 export function unzip(srcPath, destPath, callbackFn) {
   createDir(destPath); // create the folder first
 
@@ -37,6 +48,10 @@ export function unzip(srcPath, destPath, callbackFn) {
   });
 }
 
+/**
+ * Deletes an existing directory
+ * @param {string} directory - directory path to delete
+ */
 export function removeDir(directory) {
   fs.rmSync(directory, { recursive: true, force: true }, (error) => {
     if (error) {
