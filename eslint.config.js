122a07683fb73ee3409e2be30853608e9b197f6d
Aden Chua <37574101+adenchua@users.noreply.github.com>
2024-10-31 10:38:03 +0800

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+];
